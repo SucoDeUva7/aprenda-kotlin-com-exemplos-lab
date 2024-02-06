b9888d816bb5eefc487b2b5b03872648291f4297
Gabriel 'Grape Juice' Henrique Silva <83983736+SucoDeUva7@users.noreply.github.com>
2024-02-06 15:25:24 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,4 +1,4 @@
-// Playground 
+// Playground https://pl.kotl.in/0I-RnGRg5
 
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
@@ -19,6 +19,7 @@ data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<Cont
         println("Usuario ${usuario.nome} foi matriculado na formação $nome.")
     }
         // TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        
     fun exibirFormacao() {
         println("Formação: $nome, Nivel: $nivel")
         println("Conteúdos:")
@@ -48,4 +49,5 @@ fun main() {
     
    // TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
    // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+   
 }
